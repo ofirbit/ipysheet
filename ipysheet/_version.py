a813394cc8910642b6ac11532c54b137094e54b6
Ofir Yagil <ofir@imubit.com>
2020-04-28 13:14:03 +0300

    jupyter2 js on root folder

--- ipysheet/_version.py
+++
@@ -1,6 +0,0 @@
-__version_tuple__ = (0, 4, 3)
-__version_tuple_js__ = (0, 4, 3)
-__version__ = '0.4.3'
-__version_js__ = '0.4.3'
-
-version_info = __version_tuple__  # kept for backward compatibility
